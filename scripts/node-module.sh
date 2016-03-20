7e67dad3064eca10caf80c5f3c11397e3a220529
Prince J Wesley <princejohnwesley@gmail.com>
2016-03-21 03:43:42 +0530

    mancy directory for mancy-repl

--- scripts/node-module.sh
+++ scripts/node-module.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #run from app root
-rm -rf dist/ rm -rf out/ && lein cljsbuild once mancy-repl
-mkdir dist
-sed '1d' out/cljs_mancy.js > dist/clojurescript.js
-echo "module.exports = { compiler: cljs_mancy.core, goog: goog, cljs: cljs }" >> dist/clojurescript.js
+rm -rf out/ && lein cljsbuild once mancy-repl
+sed '1d' out/cljs_mancy.js > mancy/clojurescript.js
+echo "module.exports = { compiler: cljs_mancy.core, goog: goog, cljs: cljs }" >> mancy/clojurescript.js
 
